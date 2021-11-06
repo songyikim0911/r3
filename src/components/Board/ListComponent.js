6991bf4bb1c6fdbb502866d883b4982adfa19c50
songyikim0911 <ksi6470@gmail.com>
2021-11-06 21:26:41 +0900

    react project

--- src/components/Board/ListComponent.js
+++ src/components/Board/ListComponent.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {parse} from "query-string";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
+
+const pageInfoState = {
+    count:0,
+    page:1,
+    size:10,
+    prev:false,
+    next:false,
+    start:0,
+    end:0,
+    dtoList:[]
+}
+
+const range = (from, to)=>{
+    const arr = []
+    for(let i = from; i<= to; i++){
+        arr.push(i)
+    }
+    return arr
+}
 
 const ListComponent = ({location}) => {
+    console.log("----ListComponent")
+    const [pageInfo, setPageInfo] = useState(pageInfoState)
+    const [reload, setReload] = useState(false)
+
+    const history = useHistory()
 
     const params = parse(location.search)
 
-    const pageDTO = {page:params.page | 1, size:params.size | 10}
+    const pageDTO = {page:params.page||1, size:params.size||10}
 
-    console.log(params)
+    console.log(pageDTO)
+
+    useEffect(()=>{
+
+        console.log("get json data")
+        axios.get(`http://localhost:8080/api/board/list?page=${pageDTO.page}&size=${pageDTO.size}`)
+            .then(
+            response=>{
+                console.log(response.data)
+                setPageInfo(response.data)
+            }
+        )
+
+    },[pageDTO.page, reload])
+
+    const dtoLi = pageInfo.dtoList.map(dto=> <li key={dto.bno}>{dto.bno}-- {dto.title}</li>)
+
+
+
+    const movePage = (pageNum)=>{
+        //alert("pageNum:"+pageNum)
+        setReload(!reload)
+        history.push(`/board/list?page=${pageNum}`)
+    }
+
+    const pageLi = range(pageInfo.start, pageInfo.end).map(pageNum =>
+        <li key={pageNum} onClick={()=>movePage(pageNum)}>{pageNum}</li>)
 
     return (
         <div>
             <h1>BoardListComponent</h1>
+            <ul>
+                {dtoLi}
+            </ul>
+            <ul>
+                {pageLi}
+            </ul>
         </div>
     );
 };
--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,10 +1,12 @@
 import {Route, Switch} from "react-router-dom";
 import BoardListPage from "./BoardListPage";
+import BoardRegisterPage from "./BoardRegisterPage";
 
 export default () => {
     return(
         <Switch>
             <Route path="/board/list" component={BoardListPage}></Route>
+            <Route path="/board/register" component={BoardRegisterPage}></Route>
             <Route path="/board/" component={BoardListPage}></Route>
         </Switch>
     )
